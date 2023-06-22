593c6e3495664e8641f9499e4dacbc0e9cbee29e
hieuhn0301 <hieuhn0301@gmail.com>
2023-06-22 17:14:03 +0700

    Merge remote-tracking branch 'origin/HieuHN'

---
+++ app/src/main/java/com/example/prm392_finalecommerce/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.prm392_finalecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
