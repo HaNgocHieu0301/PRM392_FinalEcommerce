2425a470000b736bcdbf7ccb31dd501299d53124
hieuhn0301 <hieuhn0301@gmail.com>
2023-06-22 17:15:42 +0700

    re-push

--- app/src/main/java/com/example/prm392_finalecommerce/SignUpActivity.java
+++ app/src/main/java/com/example/prm392_finalecommerce/SignUpActivity.java
@@ -8,6 +8,8 @@ public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
     }
 }
