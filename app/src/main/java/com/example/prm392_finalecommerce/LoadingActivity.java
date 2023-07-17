255e8d8a5d18b36bd55c8850b2105b48e4b44976
hieuhn0301 <hieuhn0301@gmail.com>
2023-07-17 15:58:40 +0700

    gg services

---
+++ app/src/main/java/com/example/prm392_finalecommerce/LoadingActivity.java
@@ -0,0 +1,35 @@
+package com.example.prm392_finalecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import Repository.DataInsertionByFirebaseCallback;
+import Repository.ProductRepository;
+import Repository.UserRepository;
+
+public class LoadingActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        ProductRepository productRepository = new ProductRepository(LoadingActivity.this.getApplication());
+        productRepository.InsertDataFromFirebaseToSqlite(LoadingActivity.this.getApplication());
+        UserRepository userRepository = new UserRepository(LoadingActivity.this.getApplication());
+        userRepository.InsertDataFromFirebaseToSqlite(LoadingActivity.this.getApplication());
+
+        Button nextPage = findViewById(R.id.buttonNextToLogin);
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoadingActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
